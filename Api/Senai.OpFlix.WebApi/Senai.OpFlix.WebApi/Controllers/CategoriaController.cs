cd20f6969ca30cf0dfe70d4791ba96905b0129b9
Arthur Senna de Andrade Souza <44761640804@info.local>
2019-10-08 17:30:05 -0300

    .

---
+++ Api/Senai.OpFlix.WebApi/Senai.OpFlix.WebApi/Controllers/CategoriaController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Senai.OpFlix.WebApi.Domains;
+using Senai.OpFlix.WebApi.Repositories;
+
+namespace Senai.OpFlix.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+    public class CategoriaController : ControllerBase
+    {
+
+        CategoriaRepository CategoriaRepository = new CategoriaRepository();
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            return Ok(CategoriaRepository.ListarCategorias());
+        }
+
+
+        [Authorize(Roles = "2")]
+        [HttpPut]
+        public IActionResult Atualizar(Genero genero)
+        {
+            try
+            {
+                if (genero == null)
+                {
+                    return NotFound();
+                }
+                CategoriaRepository.AtualizarCategorias(genero);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { mensagem = "Erro. Aguarde um momento." + ex.Message });
+            }
+        }
+
+
+        [Authorize(Roles = "2")]
+        [HttpPost]
+        public IActionResult Cadastrar(Genero genero)
+        {
+            try
+            {
+                CategoriaRepository.CadastrarCategorias(genero);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(new { mensagem = "Erro. Aguarde um momento." + ex.Message });
+            }
+        }
+    }
+}
---
+++ Api/Senai.OpFlix.WebApi/Senai.OpFlix.WebApi/Interfaces/ICategoriaRepository.cs
@@ -0,0 +1,15 @@
+﻿using Senai.OpFlix.WebApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.OpFlix.WebApi.Interfaces
+{
+    interface ICategoriaRepository
+    {
+        List<Genero> ListarCategorias();
+        void CadastrarCategorias(Genero genero);
+        void AtualizarCategorias(Genero genero);
+    }
+}
