197c4be35d5fb8b7a0e4d3b4b4fd730fed8ce8dd
arthursenaa <asassena@yahoo.com.br>
2019-11-19 18:12:56 -0200

    .

--- opflixmobile/src/index.js
+++ opflixmobile/src/index.js
@@ -39,7 +39,7 @@ const MainNavigator = createBottomTabNavigator(
             inactiveBackgroundColor: '#1a1a1a',
             style: {
                 width: '100%',
-                height: 50
+                height: 50,
             }
         }
     },
